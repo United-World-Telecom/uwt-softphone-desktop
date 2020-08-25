0004a1214dde1ff61bafa8bd6a31f7cc0c631d28
Rodrigo Oliveira <rodrigo@unitedworldtelecom.com>
2020-08-25 14:09:12 -0400

    integrate auto updater

---
+++ forge.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+const fs = require('fs');
+
+fs.readFileSync('.env').toString().split('\n').forEach(kvp => {
+  const [ key, value ] = kvp.split('=');
+  process.env[key] = value;
+});
+
+require('update-electron-app')({
+  repo: 'United-World-Telecom/uwt-softphone-desktop',
+  updateInterval: '5 minutes'
+});
+
+module.exports = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "uwt_softphone"
+      }
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: [
+        "darwin"
+      ]
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        draft: false,
+        repository: {
+          owner: 'United-World-Telecom',
+          name: 'uwt-softphone-desktop',
+        }
+      }
+    }
+  ]
+}
